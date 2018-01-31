c57ded85999330ab504b81b1e9c2f2d9a2bf2caf
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-01-31 10:46:19 +0100

    Initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="bc006c3b-4b43-4907-9857-854d295b1841")
+public class DataBindingInfo {}
