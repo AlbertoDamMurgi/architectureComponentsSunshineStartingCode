322ff910804f06bdef6d7f63faad9cc93108c1af
AlbertoDamMurgi <fernandez.fernandez.angel@gmail.com>
2018-02-05 13:40:32 +0100

    asd

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="bc006c3b-4b43-4907-9857-854d295b1841")
+@BindingBuildInfo(buildId="bdf6b50d-e49d-4c90-b2f1-a1b5ac94aea4")
 public class DataBindingInfo {}
